34746b503fb5d4147e7a79944e63be46e2029702
lyzin <lyzin@xx.com>
2023-05-25 14:19:10 +0800

    mod:修改email

--- change_user.sh
+++ change_user.sh
@@ -9,19 +9,19 @@
 #
 
 git filter-branch --env-filter '
-
-OLD_EMAIL="liuyangbj01@kanyun.com"
+WRONG_EMAIL="liuyangbj01@kanyun.com"
 NEW_NAME="zinly"
-NEW_EMAIL="zinnly@xxx.com"
+NEW_EMAIL="zinly@xxx.com"
 
-if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+if [ "$GIT_COMMITTER_EMAIL" = "$WRONG_EMAIL" ]
 then
-    export GIT_COMMITTER_NAME="$CORRECT_NAME"
-    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+    export GIT_COMMITTER_NAME="$NEW_NAME"
+    export GIT_COMMITTER_EMAIL="$NEW_EMAIL"
 fi
-if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+if [ "$GIT_AUTHOR_EMAIL" = "$WRONG_EMAIL" ]
 then
-    export GIT_AUTHOR_NAME="$CORRECT_NAME"
-    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+    export GIT_AUTHOR_NAME="$NEW_NAME"
+    export GIT_AUTHOR_EMAIL="$NEW_EMAIL"
 fi
 ' --tag-name-filter cat -- --branches --tags
+
